3f8f9c52dea4f1177745f8b86d5441a9169be97a
Ciro Callazans <ccallazans@protonmail.com>
2023-11-26 16:09:56 -0300

    adicionado novas janelas de ram e disco

--- memories/FifoMemory.py
+++ memories/FifoMemory.py
@@ -1,6 +1,8 @@
 from windows.memory import *
 from tkinter import *
 from tkinter import messagebox
+import pandas as pd
+import numpy as np
 
 def janelaFifo(num_pag, janela_anterior):
     numPag = num_pag
@@ -32,22 +34,13 @@ def janelaFifo(num_pag, janela_anterior):
     def atualizar_pilha_vir():
         cont = -1
         for i in range(len(mem_virtual)):
-            if i < len(mem_virtual) and i < 8:
+            if i < len(mem_virtual) and i < 50:
                 canvas2.itemconfigure(quadrados2[i], text=i)
-            elif i >= 8:
+            elif i >= 50:
                 cont += 1
                 canvas2.itemconfigure(quadrados2[i], text=cont)     
             else:
                 canvas2.itemconfigure(quadrados2[i], text="")
-        
-    def armazenar_valores():
-        if entrada.get() != "":
-            valor = entrada.get()
-            adicionar_valor_fis(valor)
-            adicionar_valor_vir()
-            entrada.delete(0, "end")
-        else:
-            messagebox.showinfo("VALOR INVÁLIDO!", "Insira um dado não vazio!")
         
     #Adiciona valor na memoria fisica
     def adicionar_valor_fis(num): 
@@ -90,52 +83,39 @@ def janelaFifo(num_pag, janela_anterior):
 
     # Criar a janela
     window = janela_anterior
-    # window.title("Simulação de FIFO")
-    # window.geometry("750x650+500+150")
-    #window.resizable(height=False, width=False)
 
-    # Criar o canvas para exibir os quadrados de memoria fisica
-    canvas = Canvas(window, width=100, height=205)
-    canvas.place(x=340, y=130)
+
+    # Criar o canvas para exibir os quadrados de ram
+    canvas = Canvas(window, width=1300, height=60)
+    canvas.place(x=400, y=430)
     
-    titulo = Label(window, text="Memoria Fisica", anchor="center")
-    titulo.place(x=340, y=100)
+    titulo = Label(window, text="RAM", anchor="center")
+    titulo.place(x=400, y=400)
 
-    # Criar os quadrados dentro do canvas de memoria fisica
+    # Criar os quadrados dentro do canvas de ram
     quadrados = []
     x, y = 10, 10
-    for i in range(8):
-        quadrado = canvas.create_rectangle(x, y, x+80, y+20, outline="black")
+    for i in range(50):
+        quadrado = canvas.create_rectangle(x, y, x+50, y+20, outline="black")
         texto = canvas.create_text(x+40, y+10, text="")
         quadrados.append(texto)
-        y += 25
-        
-        #imprime o valor de indice ao lado da tabela
-        textoIndice = Label(window, text=i, anchor="center")
-        textoIndice.place(x=335, y= y+105)
+        x += 25
 
-    # Criar o canvas para exibir os quadrados de memoria Virtual
-    canvas2 = Canvas(window, width=100, height=420)
-    canvas2.place(x=100, y=130)
+    # Criar o canvas para exibir os quadrados de disco
+    canvas2 = Canvas(window, width=1300, height=60)
+    canvas2.place(x=400, y=580)
     
-    titulo2 = Label(window, text="Memoria Virtual", anchor="center")
-    titulo2.place(x=100, y=100)
+    titulo2 = Label(window, text="DISCO", anchor="center")
+    titulo2.place(x=400, y=550)
 
     # Criar os quadrados dentro do canvas de memoria Virtual
     quadrados2 = []
     x, y = 10, 10
-    for i in range(int(numPag)):
-        quadrado2 = canvas2.create_rectangle(x, y, x+80, y+20, outline="black")
+    for i in range(50):
+        quadrado2 = canvas2.create_rectangle(x, y, x+50, y+20, outline="black")
         texto2 = canvas2.create_text(x+40, y+10, text="")
         quadrados2.append(texto2)
-        y += 25
-
-    # Criar os widgets
-    entrada = Entry(window)
-    entrada.place(x=40, y=45)    
-
-    botao_adicionar = Button(window, text="Adicionar", command=armazenar_valores)
-    botao_adicionar.place(x=40, y=15)
+        x += 25
 
     mem_fisica   = []
     mem_virtual  = []
